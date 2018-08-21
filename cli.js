550128892f2f471ec653ad6cca8703d66109a3ad
Dario Vladovic <d.vladimyr@gmail.com>
2018-08-21 04:36:02 +0200

    Implement progress tracking :tada:
    - adding onProgress callback to read, download & package methods
    - improving CLI experience with dynamic progressbar

--- cli.js
+++ cli.js
@@ -2,10 +2,13 @@
 
 'use strict';
 
+const { Section } = require('./lib/progress');
 const createZipStream = require('.');
 const fs = require('fs');
+const Gauge = require('gauge');
 const green = require('ansi-green');
 const meow = require('meow');
+const path = require('path');
 const pkg = require('./package.json');
 
 const help = `
@@ -24,9 +27,30 @@ const flags = {
   version: { alias: 'v' }
 };
 
+const Status = {
+  [Section.READ]: 'Reading remote data',
+  [Section.DOWNLOAD]: 'Downloading storyline files',
+  [Section.PACKAGE]: 'Creating storyline package'
+};
+
 (async () => {
   const cli = meow(help, { flags });
   const [storyId, archivePath] = cli.input;
-  const stream = await createZipStream(storyId);
+  console.log(`Downloading storyline from ${green('Articulate Review')}...`);
+  const gauge = new Gauge();
+  const stream = await createZipStream(storyId, onProgress);
+  stream.on('finish', onFinish);
   stream.pipe(fs.createWriteStream(archivePath));
+
+  function onProgress(value, section) {
+    const status = Status[section] || '';
+    gauge.show(status, value);
+    gauge.pulse();
+  }
+
+  function onFinish() {
+    gauge.hide();
+    const dest = path.relative(process.cwd(), archivePath);
+    console.log(`Storyline ${green(storyId)} successfully downloaded to ${green(dest)}`);
+  }
 })();
--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const { promisify } = require('util');
 const { ZipFile } = require('yazl');
 const debug = require('debug');
 const download = require('./lib/download');
+const progress = require('./lib/progress');
 const rimraf = promisify(require('rimraf'));
 const tempy = require('tempy');
 
-module.exports = async function createZipStream(storyId) {
+const noop = Function.prototype;
+const { Section } = progress;
+
+module.exports = async function createZipStream(storyId, onProgress = noop) {
   const tempDir = tempy.directory();
-  const results = await download(storyId, tempDir);
+  const downloadTime = 0.9;
+  const results = await download(storyId, tempDir, (value, section) => {
+    onProgress(downloadTime * value, section);
+  });
   const archive = new ZipFile();
+  const packagingTime = 1 - downloadTime;
+  const notifyProgress = progress(1, Section.PACKAGE, (value, section) => {
+    onProgress(downloadTime + packagingTime * value, section);
+  });
   results.forEach(result => {
     if (!result.isFulfilled) return;
     const { localPath, path } = result.value;
@@ -18,5 +29,8 @@ module.exports = async function createZipStream(storyId) {
     debug('packager')('adding %s: %s', path, localPath);
   });
   archive.end();
-  return archive.outputStream.on('finish', () => rimraf(tempDir));
+  return archive.outputStream.on('finish', () => {
+    notifyProgress();
+    rimraf(tempDir);
+  });
 };
